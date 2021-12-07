a3f0e5d382a3fe201fec19cdef5e7479a77f67ff
Mikel Blanchard <mblanchard@macrosssoftware.com>
2021-12-07 12:08:11 -0800

    Reproduce CS1062 using ValidateCopyToArguments.

---
+++ StreamNullable/MyCustomStream.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace StreamNullable
+{
+	public sealed class MyCustomStream : MemoryStream
+	{
+		private readonly byte[] _Header = new byte[128];
+
+		public void WriteHeader(Stream destination)
+		{
+			ValidateCopyToArguments(destination, bufferSize: 128);
+
+			destination.Write(_Header);
+		}
+	}
+}
